ffb16633f9814bb3f1fcd24743cf655694968d38
Muhammad-Fahad-MF <imperial.king42@gmail.com>
2026-03-02 02:47:14 +0500

    Create main.cpp
    added main

---
+++ OOP-Assignment01/main.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string>
+
+using namespace std;
+
+enum class Category { SEDAN, SUV, }
+
+class Car{
+    private:
+        string make;
+        string model;
+        string city;
+        string category;
+
+}
+
+class User{
+    private:
+        string name;
+        long int phoneNum;
+        string email;
+        string city;
+        
+};
+
+int main(){
+
+
+
+    return 0;
+}
