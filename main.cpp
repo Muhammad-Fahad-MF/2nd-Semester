dd841a073a68fc72ca40a37e8c2a7d72aa21fbc4
Muhammad-Fahad-MF <imperial.king42@gmail.com>
2026-03-04 01:28:46 +0500

    Laptop

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ using namespace std;
 enum class Category { SEDAN, SUV, VAN };
 
 class Car{
+private:
+    const int id;
+    static int cars_count;
 public:
     string make;
     string model;
@@ -17,10 +20,53 @@ public:
     string description;
 
     string getCategory() {
-        string c = (string) category;
-        return c;
+        switch (category) {
+            case Category::SEDAN:
+                return "SEDAN";
+            case Category::SUV:
+                return "SUV";
+            case Category::VAN:
+                return "VAN";
+            default:
+                return "UNKNOWN";
+        }
+    }
+
+    Car(): id(++cars_count){
+        string cat;
+        cout << "Enter Category: ";
+        cin >> cat;
+        while(true){
+            if (cat == "SEDAN") {
+                this->category = Category::SEDAN;
+                break;
+            } else if (cat == "SUV") {
+                this->category = Category::SUV;
+                break;
+            } else if (cat == "VAN") {
+                this->category = Category::VAN;
+                break;
+            } else {
+                cout << "Invalid Category! Select( SEDAN / SUV / VAN)" << endl;
+                continue;
+            }
+        }
+        cout << "Enter make: ";
+        cin >> this->make;
+        cout << "Enter model: ";
+        cin >> this->model;
+        cout << "Enter city: ";
+        cin >> city;
+        cout << "Enter Mileage: ";
+        cin >> this->mileage;
+        cout << "Enter Price: ";
+        cin >> this->price;
+        cout << "Enter Description: ";
+        cin >> this->description;
     }
 };
+
+int Car::cars_count = 0;
 
 class User {
 private:
@@ -34,42 +80,22 @@ public:
     static vector<Car> allCars;
     bool addAds() {
         Car c;
-        cout << "Enter make: ";
-        cin >> c.make;
-        cout << "Enter model: ";
-        cin >> c.model;
-        cout << "Enter city: ";
-        cin >> city;
-        string cat;
-        cout << "Enter Category: ";
-        cin >> cat;
-        if (cat == "SEDAN") {
-            c.category = Category::SEDAN;
-        } else if (cat == "SUV") {
-            c.category = Category::SUV;
-        } else if (cat == "VAN") {
-            c.category = Category::VAN;
-        } else {
-            cout << "Invalid Category" << endl;
-            return false;
-        }
-        cout << "Enter Mileage: ";
-        cin >> c.mileage;
-        cout << "Enter Price: ";
-        cin >> c.price;
-        cout << "Enter Description: ";
-        cin >> c.description;
         ads.push_back(c);
         allCars.push_back(c); // Static member is common for all classes which will behave as all cars pool.
         return true;
     }
     static void printAllCars() {
         for (auto & allCar : allCars) {
-            cout << allCar.make << " " << allCar.model << " " << allCar.city << " " << allCar.printCategory() << " " << allCar.mileage << " Rs." << allCar.price << endl;
+            cout << allCar.make << " " << allCar.model << " " << allCar.city << " " << allCar.getCategory() << " " << allCar.mileage << " Rs." << allCar.price << endl;
         }
     }
     void buyCar() {
-
+        cout << "\nChoose One of the Cars: " << endl;
+        printAllCars();
+        int cId;
+        cout << "\nEnter a car id: ";
+        cin >> cId;
+        
     }
 };
 
@@ -87,7 +113,11 @@ public:
             cin >> choice;
             switch (choice) {
                 case 1:
-                    user.addAds();
+                    if(user.addAds()){
+                        cout << "Car added successfully!" << endl;
+                    } else {
+                        cout << "Failed to add car. Please try again." << endl;
+                    }
                 case 2:
                     user.printAllCars();
 
